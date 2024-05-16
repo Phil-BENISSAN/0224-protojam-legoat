a9ad29acc7e10b2bd6237e196fc02a55cd4d8edc
Delgorithm <delgorithm@gmail.com>
2024-05-16 15:00:33 +0200

    first commit branch signin

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,11 +1,16 @@
-import "../Styles/Header.css"
+import { Link } from "react-router-dom";
+import "../Styles/Header.css";
 
-function Header () {
-    return (
-<>
-<img className="logo" src="/Img/logo_RandoZen.svg" alt="" />
-<img className="profilPicture" src="/Img/avatar.png" alt="" />
-</>
-    )
+function Header() {
+	return (
+		<>
+			<Link to="/">
+				<img className="logo" src="/Img/logo_RandoZen.svg" alt="" />
+			</Link>
+			<Link to="/signin">
+				<img className="profilPicture" src="/Img/avatar.png" alt="" />
+			</Link>
+		</>
+	);
 }
 export default Header;
