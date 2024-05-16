b76c64417f98a5f8d3eacbd9584a90798d6163e1
Delgorithm <delgorithm@gmail.com>
2024-05-16 21:07:17 +0200

    randos

--- client/src/Pages/NewsRandos.jsx
+++ client/src/Pages/NewsRandos.jsx
@@ -1,10 +1,60 @@
+// const iconActivities = "/Img/icons8-confettis-90.png";
+const randoImg1 = "/Img/randoimg1.jpg";
+const randoImg2 = "/Img/randoimg2.jpg";
+const randoImg3 = "/Img/randoimg3.jpg";
+const randoImg4 = "/Img/randoimg4.jpg";
+
 function NewsRandos() {
 	return (
-		<section className="bg-red-200 p-4 grid grid-cols-2">
-			<section>1</section>
-			<section>2</section>
-			<section>3</section>
-			<section>4</section>
+		<section className="mt-14 p-4 grid grid-cols-2 gap-5 mb-12 text-sm">
+			<section className="border-[0.5px] border-black rounded-lg p-3">
+				<img src={randoImg1} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg2} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg3} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg4} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg1} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg2} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg3} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
+			<section className="border-[0.5px] border-black rounded-lg p-2">
+				<img src={randoImg4} alt="" className="size-28" />
+				<p>Randonée :</p>
+				<p>Durée :</p>
+				<p>Distance :</p>
+			</section>
 		</section>
 	);
 }
