d56f20239d30e64cb9cf97d011e392bf14ca466f
Delgorithm <delgorithm@gmail.com>
2024-05-16 16:49:19 +0200

    modifiction on header + Newsrandos + fastapi

--- client/src/Pages/NewsRandos.jsx
+++ client/src/Pages/NewsRandos.jsx
@@ -1,8 +1,11 @@
 function NewsRandos() {
 	return (
-		<>
-			<p>News rando</p>
-		</>
+		<section className="bg-red-200 p-4 grid grid-cols-2">
+			<section>1</section>
+			<section>2</section>
+			<section>3</section>
+			<section>4</section>
+		</section>
 	);
 }
 
