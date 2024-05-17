cad1db650781f93bad63c72a255124ab6e7c7c63
Delgorithm <delgorithm@gmail.com>
2024-05-17 02:59:53 +0200

    success page

--- client/src/Pages/Success.jsx
+++ client/src/Pages/Success.jsx
@@ -1,8 +1,8 @@
 function Success() {
 	return (
-		<>
-			<p>Succès</p>
-		</>
+		<section className="m-4 bg-red-400 h-[42rem] flex justify-center items-center">
+			<h1 className="text-3xl">Succès</h1>
+		</section>
 	);
 }
 
